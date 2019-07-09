bba48d4cc3e564b3b606233ceca995502f3f8cec
Pierre <pgfuentes@yahoo.com>
2019-07-09 01:54:20 +0000

    creating page to show more details for a single place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,23 +1,26 @@
 class PlacesController < ApplicationController
- before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create]
 
- def index
-  @places = Place.all
- end
-
- def new
-  @place = Place.new
+  def index
+    @places = Place.all
   end
 
-def create
-  current_user.places.create(place_params)
-  redirect_to root_path
-end
+  def new
+    @place = Place.new
+  end
+
+  def create
+    current_user.places.create(place_params)
+    redirect_to root_path
+  end
+
+  def show
+    @place = Place.find(params[:id])
+  end
 
   private
 
   def place_params
-    params.require(:place).permit(:name,  :description, :address)
+    params.require(:place).permit(:name, :description, :address)
   end
-
 end
