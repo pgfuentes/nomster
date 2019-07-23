e4b8eccaea1b17598eb397110b6a653287b9331f
Pierre <pgfuentes@yahoo.com>
2019-07-23 21:39:21 +0000

    updating email lesson

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,7 +3,10 @@ class NotificationMailer < ApplicationMailer
 
 
 def comment_added
-  mail(to: "my@email.com", 
+  @place = comment.place
+  @place_owner = @place.user
+
+  mail(to: "pgfuentes2015@gmail.com", 
     subject: "A comment has been added to your place")
 end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,10 +12,6 @@ config.action_mailer.delivery_method = :smtp
     password: ENV['GMAIL_PASSWORD']
   }
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
-
-
 
 
 config.action_mailer.default_url_options = { host: 'nomster-pierre-fuentes.herokuapp.com' }
